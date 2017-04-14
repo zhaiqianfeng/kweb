534f0b3fcd2a3433559629b67e84ad22912c0e23
zhaiqianfeng <zhaiqianfeng@163.com>
2017-04-14 17:40:38 +0800

    update

---
+++ code/kbase-fragment/src/test/java/com/kweb/base/test/controller/CaptchaControllerTest.java
@@ -0,0 +1,45 @@
+package com.kweb.base.test.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"classpath:spring/*.xml" })
+public class CaptchaControllerTest {
+	
+	@Autowired
+	private WebApplicationContext wac;
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+				.alwaysDo(print())
+				.build();
+	}
+
+	@Test
+	public void _000testDoGetList() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/assets/captcha"))
+				.andExpect(status().isOk());
+	}
+	
+	
+}
